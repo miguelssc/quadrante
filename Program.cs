050e6b7e9cf4e1963603d8be1ec3cd1af32624bd
miguel <miguelpositivo02@gmail.com>
2023-06-02 21:58:06 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine("- Quadrante -");
+
+ 
+
+Console.WriteLine("Digite as coordenadas para serem localizadas no quadrante. ");
+
+Console.WriteLine("Digite a coordenada X..... ");
+
+double coordenadax = Convert.ToDouble(Console.ReadLine());
+
+ 
+
+Console.WriteLine("Agora digite a coordenada y..... ");
+
+double coordenaday = Convert.ToDouble(Console.ReadLine());
+
+ 
+
+if (coordenadax > 0 && coordenaday > 0)
+
+{
+
+Console.WriteLine($"O ponto ({coordenadax}, {coordenaday}) fica no primeiro quadrante. (I)");
+
+}
+
+else if (coordenadax > 0 && coordenaday < 0)
+
+{
+
+Console.WriteLine($"O ponto ({coordenadax}, {coordenaday}) fica no quarto quadrante. (IV)");
+
+}
+
+else if (coordenadax < 0 && coordenaday > 0)
+
+{
+
+Console.WriteLine($"O ponto ({coordenadax}, {coordenaday}) fica no segundo quadrante. (II)");
+
+}
+
+else if (coordenadax < 0 && coordenaday < 0)
+
+{
+
+Console.WriteLine($"O ponto ({coordenadax}, {coordenaday}) fica no terceiro quadrante. (III)");
+
+}
+
+else
+
+{
+
+Console.WriteLine($"O ponto ({coordenadax}, {coordenaday}) fica na origem do quadrante.");
+
+}
+
